07abdbaa7ea0bd6a35f2ad42a3673de9d1127338
Sebastian Jensen <sebastianthegreatful@gmail.com>
2018-02-28 18:27:31 +0100

    Host and port can now be applied via environment for both producer and consumer

--- producer/src/main/kotlin/com/example/producer/KafkaProducerConfiguration.kt
+++ producer/src/main/kotlin/com/example/producer/KafkaProducerConfiguration.kt
@@ -1,20 +1,19 @@
 package com.example.producer
 
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.HashMap
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import java.util.*
 
 @Configuration
 class KafkaProducerConfiguration {
-
     @Value("\${kafka.host:localhost}")
-    private val kafkaHost: String? = null
+    private val host: String? = null
 
     @Value("\${kafka.port:9092}")
     private val port: Int = 0
@@ -22,7 +21,7 @@ class KafkaProducerConfiguration {
     @Bean
     fun producerFactory(): ProducerFactory<String, String> {
         val configProps = HashMap<String, Any>()
-        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "$kafkaHost:$port"
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "$host:$port"
         configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         return DefaultKafkaProducerFactory(configProps)
